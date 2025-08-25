b1993263ef0ac161d32d5575e5d01afe722d3b9a
rey cloete <recceray@gmail.com>
2025-08-25 09:16:13 +0200

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,3 @@
-///1.Save Data
-///2.Generate HTML
-///3.Make it Interactive
-
-//Wer're generating the HTML through JS instead of copying and pasting HTML products over and over//
-
-//1.Save Data ...This is a data structure//
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090 //JS has trouble with decimals, so 10.90 converted to cents 1090//
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
 
 //Accumulator function//
 let productHTML = ''; 
