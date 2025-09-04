23219971368be614f8a127b691fac703e927b193
rey cloete <recceray@gmail.com>
2025-09-04 12:41:26 +0200

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,20 @@
 //My Cart/////////////
-export let cart = [{ //No need to save all the info for a product here and in the product.js, we only need the id, and then search//
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}]; 
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) { //If the cart is empty, we just give it a default value.//
+  cart = [{ //No need to save all the info for a product here and in the product.js, we only need the id, and then search//
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }]; 
+}
+
+//Cart Local Storage - Because cart is a variable, the page resets when you refresh.//
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 //Add To Cart Function////////////////
 export function addToCart(productId) {
@@ -26,6 +35,8 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+
+  saveToStorage(); //Whenever we update cart, save to storage.//
 }
 
 //Remove From Cart Function//
@@ -39,4 +50,6 @@ export function removeFromCart(productId) { //The productId we want to remove, w
   });
 
   cart = newCart;
+
+  saveToStorage(); //Whenever we delete cart item, save to storage.//
 }
