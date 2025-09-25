5cac636b592ca91fa705b493d1889e0752b736c5
rey cloete <recceray@gmail.com>
2025-09-25 11:35:11 +0200

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -21,7 +21,7 @@ export function renderOrderSummary () {
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
         const dateString = deliveryDate.format('dddd, MMMM D');
-        ///////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
 
         cartSummaryHTML += 
 
