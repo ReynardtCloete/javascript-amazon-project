a2c1f45d6e7ad63e72568c8fb1afea20e9426a9e
rey cloete <recceray@gmail.com>
2025-10-20 14:07:55 +0200

    js-amazon

--- test-jasmine/utils/moneyTest.js
+++ test-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 //Create Test Suite//
 describe('test suite: formatCurrency', () => {
