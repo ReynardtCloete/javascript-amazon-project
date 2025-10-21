8f9841e8c9d20bd9923291854018f36ba7319d2b
rey cloete <recceray@gmail.com>
2025-10-21 14:54:53 +0200

    js amazon

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,5 +1,18 @@
-import {addToCart} from '../../data/cart.js';
+import {addToCart, cart} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
-    
-})
+
+    it('adds existing product to the cart', () => {
+
+    });
+
+    it('adds a new product to the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => { //We're spying on localStorage and call fake :)//
+            return JSON.stringify([]); //Get it to return empty array//
+        }); 
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); //Just take first product id//
+        expect(cart.length).toEqual(1); //Sometimes there are items in cart, so we do a Mock//
+    })
+
+});
