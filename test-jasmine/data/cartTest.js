e822443be66ce8b63aff5b8d738f7bb9ce78081f
rey cloete <recceray@gmail.com>
2025-10-23 13:36:09 +0200

    js amazon

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,21 +1,44 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
+import {addToCart, cart, loadFromStorage, updateDeliveryOption} from '../../data/cart.js'; //cart is loaded here, so after we mock below, cart must be reloaded//
 
 describe('test suite: addToCart', () => {
 
     it('adds existing product to the cart', () => {
 
+        spyOn(localStorage, 'setItem');
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1',
+            }]);
+        });
+ 
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); //Just take random product id//
+        expect(cart.length).toEqual(1); 
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1); //Check how many times this mocked method was called//
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(2);
+
     });
 
     it('adds a new product to the cart', () => {
+        //Also mock local storage.set item, we don't want to affect real local storage//
+        spyOn(localStorage, 'setItem');
 
         spyOn(localStorage, 'getItem').and.callFake(() => { 
             return JSON.stringify([]);  
         }); 
-        loadFromStorage(); //When this is called here in test, the spy intercepts it and fakes the result, returning [] in the cart//
-        //[] is a truthy value so second part of this function (!cart) does not run. the 2 default items are overwritten by empty array//
 
-        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); //Just take first product id//
-        expect(cart.length).toEqual(1); //Sometimes there are items in cart, so we do a Mock//
-    })
+        loadFromStorage(); //Reload cart to bring the fake into effect, setting cart to []//
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); //Just take random product id//
+        expect(cart.length).toEqual(1); 
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1); //Check how many times this mocked method was called//
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(1);
+    });
 
 });
