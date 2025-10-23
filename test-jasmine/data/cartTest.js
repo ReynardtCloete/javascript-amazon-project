68f1650349c2e830d76eb78a60ccc9c9b542194a
rey cloete <recceray@gmail.com>
2025-10-23 10:54:49 +0200

    js amazon

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -8,10 +8,11 @@ describe('test suite: addToCart', () => {
 
     it('adds a new product to the cart', () => {
 
-        spyOn(localStorage, 'getItem').and.callFake(() => { //We're spying.. on.. localStorage and.. call.. fake :)//
-            return JSON.stringify([]); //Get it to return empty array//
+        spyOn(localStorage, 'getItem').and.callFake(() => { 
+            return JSON.stringify([]);  
         }); 
-        loadFromStorage(); //This somehow doesn't return the 2 default objects from cart.js but the [] string above in the fake//
+        loadFromStorage(); //When this is called here in test, the spy intercepts it and fakes the result, returning [] in the cart//
+        //[] is a truthy value so second part of this function (!cart) does not run. the 2 default items are overwritten by empty array//
 
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); //Just take first product id//
         expect(cart.length).toEqual(1); //Sometimes there are items in cart, so we do a Mock//
